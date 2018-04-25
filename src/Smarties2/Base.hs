91a597f513870318f377a883c0c958f07a7b4853
peter lu <chippermonky@gmail.com>
2018-04-25 09:33:18 -0700

    added new nodes

--- src/Smarties2/Base.hs
+++ src/Smarties2/Base.hs
@@ -20,15 +20,15 @@ import Data.List (find)
 
 data Status = SUCCESS | FAIL deriving (Eq, Show)
 
-data UtilityNode g p o =  UtilityNode {
+data UtilityNode g p o = UtilityNode {
 	utility :: g -> p -> (Float, g)
 }
 
-data PerceptionNode g p o =  PerceptionNode {
+data PerceptionNode g p o = PerceptionNode {
 	perception :: g -> p -> (g, p)
 }
 
-data ActionNode g p o =   ActionNode {
+data ActionNode g p o = ActionNode {
 	action :: g -> p -> (g, o)	
 }
 
@@ -37,6 +37,8 @@ makeUtility (UtilityNode n) = NodeSequence fun where
 	fun g p = (u, g', p, SUCCESS, []) where
 		(u, g') = n g p
 
+-- |
+-- TODO add a (scope :: Bool) input parameter
 data NodeSequence g p o a =  NodeSequence { runNodes :: g -> p -> (a, g, p, Status, [o]) }
 
 -- this looks a lot like StateT Writer
@@ -70,7 +72,7 @@ instance (RandomGen g) => MonadRandom (NodeSequence g p o) where
     
 
 
-instance  Alternative (NodeSequence g p o) where
+instance Alternative (NodeSequence g p o) where
 	--empty :: NodeSequence g p o a
 	empty = NodeSequence func where
 		func g p = (error "trying to pull value from a guard", g, p, FAIL, [])
@@ -89,23 +91,54 @@ instance  Applicative (NodeSequence g p o) where
 		b <- n2
 		return $ f a b
 
+-- | NodeSequence builder helper 
 getState ::   NodeSequence g p o p
 getState = NodeSequence $ (\g p -> (p, g, p, SUCCESS, []))
 
+-- | NodeSequence builder helper 
 getGenerator ::  NodeSequence g p o g
 getGenerator = NodeSequence $ (\g p -> (g, g, p, SUCCESS, []))
 
 setGenerator :: (RandomGen g) => g -> NodeSequence g p o ()
 setGenerator g = NodeSequence $ (\_ p -> ((), g, p, SUCCESS, []))
 
-makeSequence :: (TreeState p) => NodeSequence g p o a -> NodeSequence g p o a 
-makeSequence ns = NodeSequence func where
+
+sequence :: (TreeState p) => NodeSequence g p o a -> NodeSequence g p o a 
+sequence ns = NodeSequence func where
 		func g p = over _3 stackPop $ (runNodes ns) g (stackPush p)
 
 selector :: (TreeState p) => [NodeSequence g p o a] -> NodeSequence g p o a
 selector ns = NodeSequence func where 
 	func g p = over _3 stackPop $ (runNodes selectedNode) g (stackPush p) where
 		selectedNode = fromMaybe empty $ find (\(NodeSequence n) -> (\case (_,_,_,x,_)-> x == SUCCESS) $ n g p) ns
+
+weightedSelection :: (RandomGen g, Ord w, Random w, Num w) => g -> [(w,a)] -> (Maybe a, g)
+weightedSelection g ns = r where
+	zero = fromInteger 0
+	total = foldl (\acc x -> fst x + acc) zero ns 
+	(rn, g') = randomR (zero, total) g
+	r = case find (\(w, _) -> w >= rn) ns of
+		Just (_,n) -> (Just n, g') 
+		Nothing -> (Nothing, g')
+
+weightedSelector :: (RandomGen g, TreeState p, Ord w, Num w, Random w) => [(w, NodeSequence g p o a)] -> NodeSequence g p o a
+weightedSelector ns = NodeSequence func where  
+	func g p = over _3 stackPop $ (runNodes selectedNode) g' (stackPush p) where
+		(msn, g') = weightedSelection g ns
+		selectedNode = fromMaybe empty msn
+
+result :: Status -> NodeSequence g p o ()
+result s = NodeSequence (\g p -> ((), g, p, s, []))
+
+flipResult :: NodeSequence g p o a -> NodeSequence g p o a
+flipResult n = NodeSequence func where
+		func g p = over _4 flipr $ (runNodes n) g p 
+		flipr s = if s == SUCCESS then FAIL else SUCCESS
+
+rand :: (RandomGen g) => Float -> NodeSequence g p o ()
+rand rn = do
+	r <- getRandomR (0,1)
+	guard (r < rn)
 
 {-
 makeSequence $ do
