ac4f7535446b8331917a293f9ea355ab4b9f7da8
peter lu <chippermonky@gmail.com>
2018-04-25 07:58:54 -0700

    bad state, reverting quantification on g

--- src/Smarties2/Base.hs
+++ src/Smarties2/Base.hs
@@ -21,28 +21,26 @@ import Data.List (find)
 data Status = SUCCESS | FAIL deriving (Eq, Show)
 
 data UtilityNode g p o = UtilityNode {
-	utility :: g -> p -> (Float, g)
+	utility :: (RandomGen g) => g -> p -> (Float, g)
 }
 
 data PerceptionNode g p o = PerceptionNode {
-	perception :: g -> p -> (g, p)
+	perception :: (RandomGen g) => g -> p -> (g, p)
 }
 
 data ActionNode g p o =  ActionNode {
-	action :: g -> p -> (g, o)	
+	action :: (RandomGen g) => g -> p -> (g, o)	
 }
 
-makeUtility :: UtilityNode p o -> NodeSequence p o Float
+makeUtility :: UtilityNode p o -> NodeSequence g p o Float
 makeUtility (UtilityNode n) = NodeSequence fun where
 	fun g p = (u, g', SUCCESS, p, []) where
 		(u, g') = n g p
 
---data NodeSequence p o a = NodeSequence { runNodes :: forall g. (RandomGen g) => g -> p -> (a, g, Status, p, [o]) }
-data NodeSequence p o a where
-	NodeSequence :: (forall g. RandomGen g) => (g -> p -> (a, g, Status, p, [o])) -> NodeSequence p o a
+data NodeSequence g p o a = NodeSequence { runNodes :: g -> p -> (a, g, Status, p, [o]) }
 
 -- this looks a lot like StateT Writer
-instance Monad (NodeSequence p o) where
+instance Monad (NodeSequence g p o) where
 	-- should only ever be used by sequence type nodes
 	NodeSequence n >>= f = NodeSequence func where
 		-- if we fail, abort the update but pass on the output and state vars
@@ -71,18 +69,18 @@ instance Monad (NodeSequence p o) where
     -}
 
 
-instance Alternative (NodeSequence p o) where
-	empty :: NodeSequence p o a
+instance Alternative (NodeSequence g p o) where
+	--empty :: NodeSequence g p o a
 	empty = NodeSequence func where
 		func g p = (error "trying to pull value from a guard", g, FAIL, p, [])
 	a <|> b = a >>= \_ -> b
 
-instance Functor (NodeSequence p o) where
+instance Functor (NodeSequence g p o) where
 	fmap f n = do 
 		a <- n
 		return $ f a
 
-instance Applicative (NodeSequence p o) where
+instance Applicative (NodeSequence g p o) where
 	pure a = NodeSequence (\g p -> (a, g, SUCCESS, p, []))
 	-- we should do something naughty here instead of reusing >>=
 	liftA2 f n1 n2 = do 
@@ -91,7 +89,7 @@ instance Applicative (NodeSequence p o) where
 		return $ f a b
 
 
-getState :: NodeSequence p o p
+getState :: NodeSequence g p o p
 getState = NodeSequence $ (\g p -> (p, g, SUCCESS, p, []))
 
 --getGenerator :: (RandomGen g) => NodeSequence g p o g
@@ -101,10 +99,10 @@ getState = NodeSequence $ (\g p -> (p, g, SUCCESS, p, []))
 --setGenerator g = NodeSequence $ (\_ p -> ((), g, SUCCESS, p, []))
 
 
-makeSequence :: NodeSequence p o a -> NodeSequence p o a 
+makeSequence :: NodeSequence g p o a -> NodeSequence g p o a 
 makeSequence = id
 
-selector :: [NodeSequence p o a] -> NodeSequence p o a
+selector :: [NodeSequence g p o a] -> NodeSequence g p o a
 selector ns = NodeSequence fun where 
 	fun g p = fromMaybe empty $ find (\NodeSequence n -> n p g == (_,_,SUCCESS,_,_))
 
