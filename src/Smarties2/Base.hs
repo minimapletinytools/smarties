e1e5cf62aefa2943f1b6d189f665b2ae66998f27
peter lu <chippermonky@gmail.com>
2018-04-25 08:53:07 -0700

    shuffled params around

--- src/Smarties2/Base.hs
+++ src/Smarties2/Base.hs
@@ -34,10 +34,10 @@ data ActionNode g p o =   ActionNode {
 
 makeUtility ::  UtilityNode g p o -> NodeSequence g p o Float
 makeUtility (UtilityNode n) = NodeSequence fun where
-	fun g p = (u, g', SUCCESS, p, []) where
+	fun g p = (u, g', p, SUCCESS, []) where
 		(u, g') = n g p
 
-data NodeSequence g p o a =  NodeSequence { runNodes :: g -> p -> (a, g, Status, p, [o]) }
+data NodeSequence g p o a =  NodeSequence { runNodes :: g -> p -> (a, g, p, Status, [o]) }
 
 -- this looks a lot like StateT Writer
 instance  Monad (NodeSequence g p o) where
@@ -46,8 +46,8 @@ instance  Monad (NodeSequence g p o) where
 	NodeSequence n >>= f = NodeSequence func where
 		-- if we fail, abort the update but pass on the output and state vars
 		-- otherwise keep going
-		func g p = if s == FAIL then (b, g', FAIL, p, os) else keepGoing where 
-			(a, g', s, p', os) = n g p -- run original node, assume it succeded
+		func g p = if s == FAIL then (b, g', p, FAIL, os) else keepGoing where 
+			(a, g', p', s, os) = n g p -- run original node, assume it succeded
 			NodeSequence n' = f a -- generate the next node
 			keepGoing = over _5 (++os) (n' g' p') -- run the next node
 			(b,_,_,_,_) = keepGoing
@@ -73,7 +73,7 @@ instance (RandomGen g) => MonadRandom (NodeSequence g p o) where
 instance  Alternative (NodeSequence g p o) where
 	--empty :: NodeSequence g p o a
 	empty = NodeSequence func where
-		func g p = (error "trying to pull value from a guard", g, FAIL, p, [])
+		func g p = (error "trying to pull value from a guard", g, p, FAIL, [])
 	a <|> b = a >>= \_ -> b
 
 instance  Functor (NodeSequence g p o) where
@@ -82,7 +82,7 @@ instance  Functor (NodeSequence g p o) where
 		return $ f a
 
 instance  Applicative (NodeSequence g p o) where
-	pure a = NodeSequence (\g p -> (a, g, SUCCESS, p, []))
+	pure a = NodeSequence (\g p -> (a, g, p, SUCCESS, []))
 	-- we should do something naughty here instead of reusing >>=
 	liftA2 f n1 n2 = do 
 		a <- n1
@@ -90,21 +90,22 @@ instance  Applicative (NodeSequence g p o) where
 		return $ f a b
 
 getState ::   NodeSequence g p o p
-getState = NodeSequence $ (\g p -> (p, g, SUCCESS, p, []))
+getState = NodeSequence $ (\g p -> (p, g, p, SUCCESS, []))
 
 getGenerator ::  NodeSequence g p o g
-getGenerator = NodeSequence $ (\g p -> (g, g, SUCCESS, p, []))
+getGenerator = NodeSequence $ (\g p -> (g, g, p, SUCCESS, []))
 
 setGenerator :: (RandomGen g) => g -> NodeSequence g p o ()
-setGenerator g = NodeSequence $ (\_ p -> ((), g, SUCCESS, p, []))
+setGenerator g = NodeSequence $ (\_ p -> ((), g, p, SUCCESS, []))
 
-makeSequence :: (RandomGen g) => NodeSequence g p o a -> NodeSequence g p o a 
-makeSequence = id
+makeSequence :: (RandomGen g, TreeState p) => NodeSequence g p o a -> NodeSequence g p o a 
+makeSequence ns = NodeSequence func where
+		func g p = over _3 stackPop $ (runNodes ns) g (stackPush p)
 
-selector ::  [NodeSequence g p o a] -> NodeSequence g p o a
-selector ns = NodeSequence fun where 
-	fun g p = (runNodes selectedNode) g p where
-		selectedNode = fromMaybe empty $ find (\(NodeSequence n) -> (\case (_,_,x,_,_)-> x == SUCCESS) $ n g p) ns
+selector :: [NodeSequence g p o a] -> NodeSequence g p o a
+selector ns = NodeSequence func where 
+	func g p = (runNodes selectedNode) g p where
+		selectedNode = fromMaybe empty $ find (\(NodeSequence n) -> (\case (_,_,_,x,_)-> x == SUCCESS) $ n g p) ns
 
 {-
 makeSequence $ do
