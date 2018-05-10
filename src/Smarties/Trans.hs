8c2a32bcbd3f45b4b680a69fcdbc675051dcb4c4
peter lu <chippermonky@gmail.com>
2018-05-10 16:01:37 -0700

    comments

--- src/Smarties/Trans.hs
+++ src/Smarties/Trans.hs
@@ -1,3 +1,15 @@
+{-|
+Module      : Smarties.Trans
+Description : Same as Smarties except with monad transformer.
+Copyright   : (c) Peter Lu, 2018
+License     : GPL-3
+Maintainer  : chippermonky@email.com
+Stability   : experimental
+
+Using NodeSequence here should be identical to using the one in Smarties.
+Implemented as 2 separate modules for performance testing reasons.
+
+-}
 module Smarties.Trans (
     module Smarties.Trans.Base,
     module Smarties.Trans.Nodes,
